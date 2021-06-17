e5fa6dd72d199bd61f96570a8a677662a06d0698
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-17 18:14:34 +0700

    [reAdd] MySort and MySortTest

--- src/MySort.java
+++ src/MySort.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySort {
+
+    private final List<Integer> input;
+
+    public MySort(List<Integer> input) {
+        this.input = input;
+    }
+
+
+    public List<Integer> sort() {
+        List<Integer> result = new ArrayList<>();
+        int temp;
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = i + 1; j < input.size(); j++) {
+                if (input.get(i) > input.get(j)) {
+                    temp = input.get(i);
+                    input.set(i, input.get(j));
+                    input.set(j, temp);
+                }
+            }
+        }
+        return input;
+    }
 }
+
+
+
--- src/MySortTest.java
+++ src/MySortTest.java
@@ -1,5 +1,158 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySortTest {
+    List<Integer> input = new ArrayList<>();
+    List<Integer> expect = new ArrayList<>();
+    List<Integer> result=new ArrayList<>();
 
+    public void clearList() {
+        input.clear();
+        expect.clear();
+        result.clear();
+    }
+
+
+    @Test
+    @DisplayName("Empty ")
+    public void emptyList() {
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 1 ")
+    public void one() {
+        clearList();
+        input.add(1);
+        expect.add(1);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 1 , 2 ")
+    public void twoValOneTwo() {
+        clearList();
+
+        input.add(1);
+        input.add(2);
+        expect.add(1);
+        expect.add(2);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 2 , 1 ")
+    public void twoValTwoOne() {
+
+        clearList();
+        input.add(2);
+        input.add(1);
+        expect.add(1);
+        expect.add(2);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 1 , 2 , 3 ")
+    public void threeValOneTwoThree() {
+        clearList();
+
+        input.add(1);
+        input.add(2);
+        input.add(3);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 2 , 1 , 3 ")
+    public void threeValTwoOneThree() {
+        clearList();
+
+        input.add(2);
+        input.add(1);
+        input.add(3);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 1 , 3 , 2 ")
+    public void threeValOneThreeTwo() {
+        clearList();
+
+        input.add(1);
+        input.add(3);
+        input.add(2);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 3 , 2 , 1 ")
+    public void threeVal() {
+        clearList();
+
+        input.add(3);
+        input.add(2);
+        input.add(1);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    @DisplayName("Input 5 , 4 , 3 , 2 , 1 ")
+    public void fiveVal() {
+        clearList();
+
+        input.add(5);
+        input.add(4);
+        input.add(3);
+        input.add(2);
+        input.add(1);
+        expect.add(1);
+        expect.add(2);
+        expect.add(3);
+        expect.add(4);
+        expect.add(5);
+
+        MySort mySort = new MySort(input);
+        result = mySort.sort();
+        assertEquals(expect, result);
+    }
 }
