bb97c35ecd787a3fb4f669df87e1f021fc744f2b
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-17 11:30:03 +0700

    add Employee main and Edit .gitifnore

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Employee {
+        private int id;
+        private String fname;
+        private String lname;
+        //    Position pos;
+        private String pos;
+
+        public String getFullName() {
+            return fname + " : " + lname;
+        }
+
+        String getPositionName() {
+            return "";
+        }
+
 }
--- src/main.java
+++ src/main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        Employee na = new Employee(); // Default constructor
+        String result = na.getFullName();
+        System.out.println(result);
+    }
+
 }
