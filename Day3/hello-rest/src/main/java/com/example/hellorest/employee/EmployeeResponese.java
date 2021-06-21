7ee3941dd23b3761bd9a6aab7fd8dab2f3888d7d
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-21 11:39:13 +0700

    edit File EmployeeResponese[json2pojp]

--- Day3/hello-rest/src/main/java/com/example/hellorest/employee/EmployeeResponese.java
+++ Day3/hello-rest/src/main/java/com/example/hellorest/employee/EmployeeResponese.java
@@ -2,14 +2,25 @@ package com.example.hellorest.employee;
 
 public class EmployeeResponese {
     private int id;
-    private  String fname;
+    private String fname;
     private String lname;
 
-    public EmployeeResponese(){
-
+    public int getId() {
+        return id;
     }
 
-    public EmployeeResponese(int id,String fname,String lname){
-        
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public EmployeeResponese(int id, String fname, String lname) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
     }
 }
+
