ed7aa2ccdae7dcee8e9924ec8fc15e646edfdaaa
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-21 11:46:46 +0700

    edit GetEmployeeByID

--- Day3/hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ Day3/hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,15 +1,16 @@
 package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
 
-    @GetMapping("/employee")
-    public EmployeeResponese getEmployeeByID(int id){
-        return new EmployeeResponese(id,"Thana","Na");
+    @GetMapping("/employee/{id}")
+    public EmployeeResponese getEmployeeByID(@PathVariable int id) {
+        return new EmployeeResponese(id, "Thana", "Na");
     }
 
 }
