1efa215a046d91ac16a0f8e19456789f358c1407
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-22 20:26:13 +0700

    add Bean Total count object

--- Day3/hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
+++ Day3/hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
@@ -2,12 +2,21 @@ package com.example.hellorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context
+				= SpringApplication.run(HelloRestApplication.class, args);
+		String[] objects = context.getBeanDefinitionNames();
+		for (String object : objects) {
+			System.out.println(object);
+		}
+        int count = context.getBeanDefinitionCount();
+        System.out.println("Total : " + count);
+
+    }
 
 }
