eef3fe8f22db9fb83034bc83f8ac51d51d893f8f
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-22 21:39:21 +0700

    Working with Mokito + Bean [Checkpoint Assg14]

--- Day3/hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
+++ Day3/hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -1,26 +1,35 @@
 package com.example.hellorest.employee;
 
+import com.example.hellorest.MyRandom.MyRandom;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    //MockBean การทำการจำลองในโลก Sring boot ทำงานร่วมกับ Mockito ได้
+    @MockBean
+    private MyRandom random;
+
 
     @Test
     public void callApiWithPathVariable() {
-        EmployeeResponese expected = new EmployeeResponese(123, "Thana", "Yok");
+        when(random.nextInt(10)).thenReturn(5);
+        EmployeeResponese expected = new EmployeeResponese(123, "Thana5", "Yok");
         EmployeeResponese response
                 = restTemplate.getForObject("/employee/123", EmployeeResponese.class);
         assertEquals(123, response.getId());
-        assertEquals("Thana5", response.getFname());
+        assertEquals("Thana", response.getFname());
         assertEquals("Yok", response.getLname());
         assertEquals(expected,response);
     }
