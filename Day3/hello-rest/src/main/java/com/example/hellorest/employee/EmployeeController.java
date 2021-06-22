8575efc7d2c93113bc1b5b54a28db3d467a5159c
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-22 18:28:47 +0700

    add RequestParam in Controller and add Test File

--- Day3/hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ Day3/hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,11 +2,11 @@ package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-
 
     @GetMapping("/employee/{id}")
     public EmployeeResponese getEmployeeByID(@PathVariable String id) {
@@ -22,5 +22,18 @@ public class EmployeeController {
         return new EmployeeResponese(_id,"Thana","Yok");
 
     }
+    // employee?id=?
+    @GetMapping("/employee")
+    public EmployeeResponese getEmployeeByID2(@RequestParam String id) {
+
+        int _id = 0;
+        try {
+            _id = Integer.parseInt(id);
+        }catch (NumberFormatException e) {
+            // ERROR => TODO ?
+        }
+        return new EmployeeResponese(_id, "Thana2", "Yok2");
+    }
+
 
 }
