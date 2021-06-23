2546f1a38b5db9be56b86453b85060f77949312b
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-23 10:06:24 +0700

    add EmployeeRepositoryTest

---
+++ Day3/hello-rest/src/test/java/com/example/hellorest/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public  void  foundWithID1(){
+        Employee employee1 = repository.getById(1);
+        assertEquals(1,employee1.getId());
+        assertEquals("Thana from DB", employee1.getFirstName());
+        assertEquals("Yok from DB", employee1.getLastName());
+    }
+
+
+
+}
