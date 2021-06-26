c872779333eb209dce4c1cfdb22811395d4e5051
Thana Yokpisutpong <thana.y@kbtg.tech>
2021-06-26 08:32:30 +0700

    data model of user api

--- Day3/hello-rest/src/main/java/com/example/hellorest/user/UserModel.java
+++ Day3/hello-rest/src/main/java/com/example/hellorest/user/UserModel.java
@@ -1,4 +1,31 @@
 package com.example.hellorest.user;
 
 public class UserModel {
+    private  int Id;
+    private String name;
+    private String email;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
